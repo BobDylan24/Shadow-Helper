12f662215e645fd6d6993c99330283586f5f0bbd
BobDylan24 <113939997+BobDylan24@users.noreply.github.com>
2023-01-12 01:24:58 +0000

    Update bot.py
    Added in reaction role command for the support server only, changed guild_Ids for load unload reload and reaction to the shadow support server guild id

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ intents.message_content = True
 
 activity = discord.Activity(type=discord.ActivityType.watching, name=f"Over /help")
 
-bot = commands.Bot(intents=intents, command_prefix="!", owner_id=[866285734808780812], activity=activity)
+bot = commands.Bot(intents=intents, command_prefix="!", owner_id=866285734808780812, activity=activity)
 
 bot.remove_command("help")
 
@@ -24,7 +24,7 @@ async def on_ready():
 async def on_ready():
     print(f"Logged into {bot.user}")
 
-@bot.slash_command(name="load", description="Loads the cog that you specify.", guild_ids=[1057799094032138301])
+@bot.slash_command(name="load", description="Loads the cog that you specify.", guild_ids=[1062880883423584298])
 @commands.is_owner()
 async def load(ctx, extension: discord.SlashCommandOptionType.string):
     bot.load_extension(f"cogs.{extension}")
@@ -40,7 +40,7 @@ async def load_error(ctx, error):
         await ctx.respond(f"```{error}```\nPlease report this error to Bob Dylan#4886 if this error continues.")
         return
 
-@bot.slash_command(name="unload", description="Unloads the cog that you specify.", guild_ids=[1057799094032138301])
+@bot.slash_command(name="unload", description="Unloads the cog that you specify.", guild_ids=[1062880883423584298])
 @commands.is_owner()
 async def unload(ctx, extension: discord.SlashCommandOptionType.string):
     bot.unload_extension(f"cogs.{extension}")
@@ -56,7 +56,7 @@ async def unload_error(ctx, error):
         await ctx.respond(f"```{error}```\nPlease report this error to Bob Dylan#4886 if this error continues.")
         return
 
-@bot.slash_command(name="reload", description="Reloads the cog that you specify.", guild_ids=[1057799094032138301])
+@bot.slash_command(name="reload", description="Reloads the cog that you specify.", guild_ids=[1062880883423584298])
 @commands.is_owner()
 async def reload(ctx, extension: discord.SlashCommandOptionType.string):
     bot.reload_extension(f"cogs.{extension}")
@@ -71,6 +71,40 @@ async def reload_error(ctx, error):
         print(error)
         await ctx.respond(f"```{error}```\nPlease report this error to Bob Dylan#4886 if this error continues.")
         return
+
+@bot.slash_command(name="reaction", description="Reaction", guild_ids=[1062880883423584298])
+@commands.is_owner()
+async def reaction(ctx):
+    class MyView(discord.ui.View):
+        @discord.ui.button(label="Bot Updates", style=discord.ButtonStyle.success)
+        async def first_button_callback(self, button, interaction):
+            role_id = 1062902392162619442
+            role = discord.utils.get(ctx.guild.roles, id=role_id)
+            await ctx.author.send("Added you to the Bot Updates Role.")
+            await bot.add_roles(ctx.author, role)
+            return
+        @discord.ui.button(label="Github Updates", style=discord.ButtonStyle.secondary)
+        async def second_button_callback(self, button, interaction):
+            role_id = 1062902911018999858
+            role = discord.utils.get(ctx.guild.roles, id=role_id)
+            await ctx.author.send("Added you to the Github Updates Role.")
+            await bot.add_roles(ctx.author, role)
+            return
+        @discord.ui.button(label="Giveaway Ping", style=discord.ButtonStyle.blurple)
+        async def third_button_callback(self, button, interaction):
+            role_id = 1062903246097743902
+            role = discord.utils.get(ctx.guild.roles, id=role_id)
+            await ctx.author.send("Added you to the Giveaway Ping Role.")
+            await bot.add_roles(ctx.author, role)
+            return
+        @discord.ui.button(label="Announcement Ping", style=discord.ButtonStyle.danger)
+        async def fourth_button_callback(self, button, interaction):
+            role_id = 1062903604937232474
+            role = discord.utils.get(ctx.guild.roles, id=role_id)
+            await ctx.author.send("Added you to the Announcement Ping Role.")
+    
+    embed = discord.Embed(title="Reaction Roles", description="Click the buttons below to assign your self the roles you want.", color=discord.Color.blurple())
+    await ctx.send(embed=embed, view=MyView())
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
