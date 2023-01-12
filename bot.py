9b6f1019621bcc6ca58c747ba2124d7e4f769a9d
BobDylan24 <113939997+BobDylan24@users.noreply.github.com>
2023-01-12 01:41:34 +0000

    Update bot.py
    Changed up the code so the interaction will be placed in the reaction role channel on bot startup just so I don't have to run the command every single time I restart the bot

--- bot.py
+++ bot.py
@@ -23,6 +23,40 @@ async def on_ready():
 @bot.event
 async def on_ready():
     print(f"Logged into {bot.user}")
+    channel = bot.get_channel(1062908409646694440)
+    await channel.purge(limit=1)
+    class MyView(discord.ui.View):
+        @discord.ui.button(label="Bot Updates", style=discord.ButtonStyle.success)
+        async def first_button_callback(self, button, interaction):
+            role_id = 1062902392162619442
+            role = discord.utils.get(ctx.guild.roles, id=role_id)
+            await interaction.response.send_message("Added you to the Bot Updates Role.", ephemeral=True)
+            await interaction.user.add_roles(role)
+            return
+        @discord.ui.button(label="Github Updates", style=discord.ButtonStyle.secondary)
+        async def second_button_callback(self, button, interaction):
+            role_id = 1062902911018999858
+            role = discord.utils.get(ctx.guild.roles, id=role_id)
+            await interaction.response.send_message("Added you to the Github Updates Role.", ephemeral=True)
+            await interaction.user.add_roles(role)
+            return
+        @discord.ui.button(label="Giveaway Ping", style=discord.ButtonStyle.blurple)
+        async def third_button_callback(self, button, interaction):
+            role_id = 1062903246097743902
+            role = discord.utils.get(ctx.guild.roles, id=role_id)
+            await interaction.response.send_message("Added you to the Giveaway Ping Role.", ephemeral=True)
+            await interaction.user.add_roles(role)
+            return
+        @discord.ui.button(label="Announcement Ping", style=discord.ButtonStyle.danger)
+        async def fourth_button_callback(self, button, interaction):
+            role_id = 1062903604937232474
+            role = discord.utils.get(ctx.guild.roles, id=role_id)
+            await interaction.response.send_message("Added you to the Announcement Ping Role.", ephemeral=True)
+            await interaction.user.add_roles(role)
+            return
+    
+    embed = discord.Embed(title="Reaction Roles", description="Click the buttons below to assign your self the roles you want.", color=discord.Color.blurple())
+    await channel.send(embed=embed, view=MyView())
 
 @bot.slash_command(name="load", description="Loads the cog that you specify.", guild_ids=[1062880883423584298])
 @commands.is_owner()
@@ -71,40 +105,6 @@ async def reload_error(ctx, error):
         print(error)
         await ctx.respond(f"```{error}```\nPlease report this error to Bob Dylan#4886 if this error continues.")
         return
-
-@bot.slash_command(name="reaction", description="Reaction", guild_ids=[1062880883423584298])
-@commands.is_owner()
-async def reaction(ctx):
-    class MyView(discord.ui.View):
-        @discord.ui.button(label="Bot Updates", style=discord.ButtonStyle.success)
-        async def first_button_callback(self, button, interaction):
-            role_id = 1062902392162619442
-            role = discord.utils.get(ctx.guild.roles, id=role_id)
-            await ctx.author.send("Added you to the Bot Updates Role.")
-            await bot.add_roles(ctx.author, role)
-            return
-        @discord.ui.button(label="Github Updates", style=discord.ButtonStyle.secondary)
-        async def second_button_callback(self, button, interaction):
-            role_id = 1062902911018999858
-            role = discord.utils.get(ctx.guild.roles, id=role_id)
-            await ctx.author.send("Added you to the Github Updates Role.")
-            await bot.add_roles(ctx.author, role)
-            return
-        @discord.ui.button(label="Giveaway Ping", style=discord.ButtonStyle.blurple)
-        async def third_button_callback(self, button, interaction):
-            role_id = 1062903246097743902
-            role = discord.utils.get(ctx.guild.roles, id=role_id)
-            await ctx.author.send("Added you to the Giveaway Ping Role.")
-            await bot.add_roles(ctx.author, role)
-            return
-        @discord.ui.button(label="Announcement Ping", style=discord.ButtonStyle.danger)
-        async def fourth_button_callback(self, button, interaction):
-            role_id = 1062903604937232474
-            role = discord.utils.get(ctx.guild.roles, id=role_id)
-            await ctx.author.send("Added you to the Announcement Ping Role.")
-    
-    embed = discord.Embed(title="Reaction Roles", description="Click the buttons below to assign your self the roles you want.", color=discord.Color.blurple())
-    await ctx.send(embed=embed, view=MyView())
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
