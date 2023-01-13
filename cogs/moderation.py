28b84b07be99eaa64c7a5337ed0bf91235e6c927
BobDylan24 <113939997+BobDylan24@users.noreply.github.com>
2023-01-13 00:32:36 +0000

    Update moderation.py
    Added the purge error handler and a new command called warn. You can warn a member with a reason and the warn will get stored in the mongo database. the more warns the higher your warn number goes. Also created the error handler for the warn command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -160,6 +160,60 @@ class moderation(commands.Cog):
             embed.add_field(name="Amount", value=f"{amount} message(s)", inline=True)
             await ctx.channel.purge(limit=amount)
             await ctx.respond(embed=embed, delete_after=10)
+    
+    @purge.error
+    async def purge_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.respond("You are missing the `manage_messages` permission.")
+            return
+        else:
+            print(error)
+            await ctx.respond(f"```{error}```\nPlease report this error to Bob Dylan#4886 if this error continues.")
+            return
+
+    @commands.slash_command(name="warn", description="Warns a member that you specify for the reason you specify.")
+    @commands.has_guild_permissions(kick_members=True)
+    async def warn(self, ctx, member : discord.SlashCommandOptionType.user):
+        current_warn_count = len(await self.bot.warns.find_many_by_custom({"user_id": member.id, "guild_id": member.guild.id})) + 1
+        ting = self.bot
+        class MyModal(discord.ui.Modal):
+            def __init__(self, *args, **kwargs) -> None:
+                super().__init__(*args, **kwargs)
+
+                self.add_item(discord.ui.InputText(label="Reason", style=discord.InputTextStyle.long))
+            async def callback(self, interaction: discord.Interaction):
+                warn_filter = {"user_id": member.id, "guild_id": member.guild.id, "number": current_warn_count}
+                warn_data = {"reason": self.children[0].value, "timestamp": today, "warned_by": ctx.author.id}
+
+                await ting.warns.upsert_custom(warn_filter, warn_data)
+
+                embed = discord.Embed(title="Warned Successfully!", description="I have successfully warned the member", color=discord.Color.green())
+                embed.add_field(name="Member Name", value=f"{member.mention}", inline=False)
+                embed.add_field(name="Reason", value=f"{self.children[0].value}", inline=False)
+                embed.set_footer(text=f"Warn: {current_warn_count}")
+                await ctx.respond(embed=embed)
+                embed = discord.Embed(title="You Have Been Warned", description=f"You have been warned in the guild {ctx.guild.name}", color=discord.Color.red())
+                embed.add_field(name="Staff Member", value=f"{ctx.author.mention}", inline=False)
+                embed.add_field(name="Reason", value=f"{self.children[0].value}", inline=False)
+                embed.add_field(name="Date", value=today, inline=False)
+                embed.set_footer(text=f"Warn: {current_warn_count}")
+                await member.send(embed=embed)
+                await interaction.response.send_message("User warned", ephemeral=True)
+        modall = MyModal(title="Why do you want to warn this user?")
+        await ctx.send_modal(modall)
+    
+    @warn.error
+    async def warn_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.respond("You are missing the `kick_members` permission.")
+            return
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.respond("The user you are trying to warn has private messages turned off.\nThe user has been warned but has not been sent a message in dms.")
+            return
+        else:
+            print(error)
+            await ctx.respond(f"```{error}```\nPlease report this error to Bob Dylan#4886 if this error continues.")
+            return
 
 def setup(bot):
     bot.add_cog(moderation(bot))
